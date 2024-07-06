8ab564a932694a7d851c34a769cd3fa50f40b089
scrriq <gdien.hc@gmail.com>
2024-07-06 15:33:12 +0200

    Enabling permissions and creating a DataClass *A DataClass has been added that will receive weather information *Creating an extensions function that will handle the permission  state *Implementation of verification and permission request

---
+++ app/src/main/java/com/example/weatherapp/fragments/Extensions.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp.fragments
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+fun Fragment.isPermissionGranted(p: String): Boolean { // p - Pemission name
+    return ContextCompat.checkSelfPermission(activity as AppCompatActivity, p) == PackageManager.PERMISSION_GRANTED
+}
