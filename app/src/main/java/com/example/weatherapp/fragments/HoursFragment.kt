e14d1e087624209dd5857c395308dcd14d3d6bf2
scrriq <gdien.hc@gmail.com>
2024-07-07 20:42:34 +0200

    The RecyclerView model has been added to display elements of the weather status by the hour

--- app/src/main/java/com/example/weatherapp/fragments/HoursFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/HoursFragment.kt
@@ -5,16 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherapp.R
+import com.example.weatherapp.adapters.WeatherAdapter
+import com.example.weatherapp.adapters.WeatherModel
+import com.example.weatherapp.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
-
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel("","12:00","Sunny","","25°C","","",""),
+            WeatherModel("","13:00","Sunny","","27°C","","",""),
+            WeatherModel("","14:00","Sunny","","35°C","","","")
+        )
+        adapter.submitList(list)
     }
 
     companion object {
